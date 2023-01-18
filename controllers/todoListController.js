2f1f4f4cc7928b6433c56f5ca235395693b92a16
Leonardo Sanchez Flores <blamefulhydra@hotmail.com>
2023-01-18 12:41:08 -0700

    Comentarios en métodos y router.

--- controllers/todoListController.js
+++ controllers/todoListController.js
@@ -1,9 +1,12 @@
 import mssql from "../conexion.js";
 import moment from 'moment';
 
+/**
+ * @description Método para obtener las tareas no completadas y devolverlas al front.
+ */
 export const getTareas = async (req, res) => {
     try {
-        const tareas = await mssql.query('Select TareaID, Descripcion, Completada from Tarea');
+        const tareas = await mssql.query('Select TareaID, Descripcion, Completada from Tarea where Completada = 0');
     
         return res.status(200).send(tareas.recordsets[0]);
     } catch (error) {
@@ -11,6 +14,9 @@ export const getTareas = async (req, res) => {
     }
 }
 
+/**
+ * @description Método para insertar la nueva tarea a la tabla de SQL Server.
+ */
 export const addTarea = async (req, res) => {
     try {
         const { tarea } = req.body
--- routes/todoList.js
+++ routes/todoList.js
@@ -3,8 +3,14 @@ import { getTareas, addTarea } from '../controllers/todoListController.js';
 
 const router = Router();
 
+/**
+ * @description Ruta base del TodoList con método HTTP GET
+ */
 router.get('/', (req, res) => getTareas(req, res));
 
+/**
+ * @description Ruta base del TodoList con método HTTP POST
+ */
 router.post('/', (req, res) => addTarea(req, res));
 
 export default router;
